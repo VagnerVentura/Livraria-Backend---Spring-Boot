46464833b7e5d66eedba31f54703c4b75cae8d6b
vagner ventura <vagnerventura@hotmail.com>
2025-05-14 11:02:38 -0300

    feat(service): criar CategoryService com camada de abstração para lógica de negócio

---
+++ src/main/java/com/Api/Livraria/services/CategoryService.java
@@ -0,0 +1,37 @@
+package com.Api.Livraria.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Api.Livraria.dto.CategoryDto;
+import com.Api.Livraria.entities.Category;
+import com.Api.Livraria.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	public Category createCategory(CategoryDto dto) {
+		Category category = new Category();
+		category.setName(dto.getName());
+		return categoryRepository.save(category);
+	}
+		
+	public List<Category> createMany(List<CategoryDto> categories) {
+		List<Category> saved = categories.stream()
+				.map(dto -> createCategory(dto))
+				.toList();
+		return saved;
+	}
+	
+	public List<Category> findCategoryById(List<Long> id) {
+		List<Category> categories = categoryRepository.findAllById(id);
+		return categories;
+	}
+
+	
+}
