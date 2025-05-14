001d85306f48845e83cac736ca52713d3a273c71
vagner ventura <vagnerventura@hotmail.com>
2025-05-13 23:50:21 -0300

    Mudando estrutura de Book e BookDtp

--- src/main/java/com/Api/Livraria/dto/BookDto.java
+++ src/main/java/com/Api/Livraria/dto/BookDto.java
@@ -1,15 +1,50 @@
 package com.Api.Livraria.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
-public record BookDto(
-		String name,
-		String author,
-		String editor,
-		String barcode,
-		Integer stock,
-		Integer nmrPaginas,
-		BigDecimal price
-		) {
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto{
+	
+	@NotBlank
+	private String title;
+	
+	@NotBlank
+	private String isbnt;
+	
+    @NotNull
+    @PastOrPresent(message = "The publication date cannot be in the future")
+	private LocalDate publicationDate;
+	
+	@NotNull
+	@PositiveOrZero
+	private Integer stock;
+	
+	@NotNull
+	@DecimalMin(value = "0.0")
+	private BigDecimal price; 
+	
+	@NotNull
+	private Long authorId;
+	
+	@NotNull
+	private Long publisherId;
+	
+	@NotEmpty
+	private List<Long> categoriesId;
 }
--- src/main/java/com/Api/Livraria/entities/Book.java
+++ src/main/java/com/Api/Livraria/entities/Book.java
@@ -1,12 +1,22 @@
 package com.Api.Livraria.entities;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,26 +34,49 @@ import lombok.Setter;
 public class Book {
 
 	@Id
-	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank(message = "Nome é obrigatório")
-	private String name;
+	@NotBlank(message = "title é obrigatório")
+	@Column(name = "title")
+	private String title;
 	
-	@NotBlank(message = "autor é obrigatório")
-	private String author;
-	
-	@NotBlank(message = "autor é obrigatório")
-	private String editor;
-	
-	@NotNull(message = "barcode é obrigatório")
+	@NotBlank(message = "isbnt é obrigatório")
 	@Column(unique = true)
-	private String barcode;
+	private String isbnt;
 	
 	private Integer stock;
 	
-	private Integer nmrPaginas;
+	private LocalDate publicationDate;
 	
-    private BigDecimal price;
+	private BigDecimal price;
 	
+	@OneToMany(mappedBy = "book")
+	@JsonManagedReference
+	private List<OrderItem> orderItems;
+	
+	@ManyToMany
+	@JsonManagedReference
+	@JoinTable(name = "books_categories",
+	joinColumns = @JoinColumn(name = "book_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private List<Category> categories;
+	
+    @NotNull(message = "autor é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="author_id")
+    @JsonBackReference
+    private Author author;
+    
+    @NotNull(message = "editora é obrigatório")
+    @ManyToOne
+    @JoinColumn(name="publisher_id")
+    @JsonBackReference
+    private Publisher publisher;
+    
+    @OneToMany(mappedBy = "book")
+    @JsonManagedReference
+    private List<Review> reviews;
+    
 }
+
