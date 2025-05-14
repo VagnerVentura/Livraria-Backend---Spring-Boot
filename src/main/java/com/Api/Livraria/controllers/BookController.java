f288d65338ee2ea7791995f17046af561152d3d7
vagner ventura <vagnerventura@hotmail.com>
2025-05-13 23:50:29 -0300

    refatoração: atualizar BookController para refletir a nova estrutura do BookDto

--- src/main/java/com/Api/Livraria/controllers/BookController.java
+++ src/main/java/com/Api/Livraria/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.Api.Livraria.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Api.Livraria.dto.BookDto;
+import com.Api.Livraria.dto.BookResponseDto;
 import com.Api.Livraria.entities.Book;
 import com.Api.Livraria.services.BookService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -25,18 +28,23 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
-	
+
 	@PostMapping
-	public ResponseEntity<Book>  createBook(@RequestBody BookDto bookDto) {
-		Book book = bookService.createBook(bookDto);	
-		return ResponseEntity.ok(book);
+	public ResponseEntity<BookResponseDto> createBook(@RequestBody @Valid BookDto bookDto) {
+		Book book = bookService.createBook(bookDto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(bookService.toDto(book));
 	}
-	
+
+	@PostMapping("/bulk")
+	public ResponseEntity<List<BookResponseDto>> createManyBooks(@RequestBody @Valid List<BookDto> bookDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createManyBooks(bookDto));
+	}
+
 	@GetMapping
-	public ResponseEntity<List<Book>> findAll(){
+	public ResponseEntity<List<BookResponseDto>> findAll() {
 		return ResponseEntity.ok(bookService.findAll());
 	}
-	
+
 	public void deleteBook(Book book) {
 		bookService.deleteBook(book);
 	}
