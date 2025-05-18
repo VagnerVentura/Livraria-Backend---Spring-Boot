0889e59569feb83f1418761336bdacdb43c2e15e
vagner ventura <vagnerventura@hotmail.com>
2025-05-18 11:09:58 -0300

    feat: Adicionando Endpoints Buscar livro por título e Buscar livros por categoria

--- src/main/java/com/Api/Livraria/controllers/BookController.java
+++ src/main/java/com/Api/Livraria/controllers/BookController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Api.Livraria.dto.BookDto;
@@ -17,6 +19,7 @@ import com.Api.Livraria.entities.Book;
 import com.Api.Livraria.services.BookService;
 
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -44,7 +47,22 @@ public class BookController {
 	public ResponseEntity<List<BookResponseDto>> findAll() {
 		return ResponseEntity.ok(bookService.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<BookResponseDto> findBookById(@PathVariable Long id){
+		return ResponseEntity.ok().body(bookService.findBookById(id));
+	}
 
+	@GetMapping("/search")
+	public ResponseEntity <List<BookResponseDto>> findBookByTitle(@RequestParam String title){
+		return ResponseEntity.ok().body(bookService.findBookByTitle(title));
+	}
+	
+	@GetMapping("category/{categoryId}")
+	public ResponseEntity<List<BookResponseDto>> searchBooksByCategoryId(@PathVariable Long categoryId){
+		return ResponseEntity.ok().body(bookService.searchBooksByCategory(categoryId));
+	}
+	
 	public void deleteBook(Book book) {
 		bookService.deleteBook(book);
 	}
